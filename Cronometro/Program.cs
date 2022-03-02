ffbfcef72c1d9fd5d879e4f32b62acf63a083472
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-02 17:01:59 -0400

    Criei a função menu

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,17 +1,43 @@
 ﻿using System.Threading;
+Menu();
 
-Iniciar();
-
-static void Iniciar()
+static void Menu()
 {
-    int tempo = 10;
+    Console.Clear();
+    Console.WriteLine("Bem vindo a cronômetro");
+    Console.WriteLine("S = Segundo => 10s => 10 Segundos");
+    Console.WriteLine("M = Minuto => 1m => 1 Minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Escolha uma opção");
+
+    //Coleta os dados escolhidos pelo usuário e converte para minúsculas.
+    string dado = Console.ReadLine().ToLower();
+
+    //Seleciona o último caractere de dado.
+    //Substring podemos pegar uma parte da cadeia de caracteres.
+    //data.lenght conta o total de caracteres de uma string, nos diz o cumprimento. 
+    char tipo = char.Parse(dado.Substring(dado.Length - 1, 1));
+
+    Console.WriteLine(dado);
+    Console.WriteLine(tipo);
+
+}
+
+static void Iniciar(int tempo)
+{
     int tempoinicial = 0;
 
     while (tempoinicial != tempo)
     {
+
         Console.Clear();
         tempoinicial++;
         Console.WriteLine(tempoinicial);
+
+        //pausa o programa temporariamente.
         Thread.Sleep(1000);
+        Console.WriteLine("Cronômetro finalizado");
+        Thread.Sleep(2500);
+        Menu();
     }
 }
