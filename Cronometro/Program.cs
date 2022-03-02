b05892d1450577610725a257a34b451930a79c20
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-02 18:56:44 -0400

    Atualizai cronometro
    O Menu identifica a resposta e chama a função iniciar ou finaliza o programa

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -18,9 +18,24 @@ static void Menu()
     //data.lenght conta o total de caracteres de uma string, nos diz o cumprimento. 
     char tipo = char.Parse(dado.Substring(dado.Length - 1, 1));
 
-    Console.WriteLine(dado);
-    Console.WriteLine(tipo);
+    //Seleciona todos os caracteres de dado menos o último.
+    int tempo = int.Parse(dado.Substring(0, dado.Length - 1));
 
+
+    //Nosso cronômetro avança a cada segundo
+    //O programa começa em "s", com o multiplicador valendo 1.
+    int multiplicador = 1;
+
+    //Caso o usuário escolha "m", o multiplicador valerá 60.
+    if (tipo == 'm')
+        multiplicador = 60;
+
+    //Se o usuário escolher 0 o programa será encerrado.
+    if (tempo == 0)
+        System.Environment.Exit(0);
+
+    //Chamando o contador.
+    Iniciar(tempo * multiplicador);
 }
 
 static void Iniciar(int tempo)
@@ -34,9 +49,10 @@ static void Iniciar(int tempo)
         tempoinicial++;
         Console.WriteLine(tempoinicial);
 
-        //pausa o programa temporariamente.
+        //pausa o programa por um segundo.
         Thread.Sleep(1000);
     }
     Console.WriteLine("Cronômetro finalizado");
     Thread.Sleep(2500);
+    Menu();
 }
