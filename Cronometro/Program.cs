7ba7634e334ead36e074beae843d565c8a2da8bf
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-02 12:27:23 -0400

    Criei a função Iniciar

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.Clear();
+﻿using System.Threading;
 
-static void Start()
+Iniciar();
+
+static void Iniciar()
 {
     int tempo = 10;
     int tempoinicial = 0;
 
+    while (tempoinicial != tempo)
+    {
+        Console.Clear();
+        tempoinicial++;
+        Console.WriteLine(tempoinicial);
+        Thread.Sleep(1000);
+    }
 }
