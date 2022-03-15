0a80ddf902de9e950e2b1ac82e19235f84c0b55a
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-03-14 21:54:53 -0400

    criei palavraformatada

---
+++ PalavraFormatada/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Digite o texto que deseja formatar");
+
+var palavra = Console.ReadLine();
+palavra = palavra.ToLower();
+
+var palavraFormatada = "";
+var colocarLetraEmMaiusculo = true;
+
+foreach (var letra in palavra)
+{
+    var novaLetra = letra.ToString();
+
+    if (colocarLetraEmMaiusculo)
+        novaLetra = novaLetra.ToUpper();
+
+    palavraFormatada = palavraFormatada + novaLetra;
+    colocarLetraEmMaiusculo = !colocarLetraEmMaiusculo;
+}
+
+Console.WriteLine(palavraFormatada);
