0ca5f1363e105d3c44a995e2d00fced1664407db
devAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-04-12 17:23:06 -0400

    atualizei string

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,7 +1,16 @@
-﻿//guids
+﻿//------------------------guids------------------------
+
 var id = Guid.NewGuid();
 id.ToString();
-
 id = new Guid("ce76155a-7d90-47b7-989b-c39f9b80693c");
-Console.WriteLine(id.ToString().Substring(0,8));
-//
+//Console.WriteLine(id.ToString().Substring(0,8));
+
+//---------------interpolação de strings---------------
+
+var price = 10.50;
+//var texto = string.Format("O preço do produto é {0} apenas na promoção", price);
+var texto = $@"O preço do produto é {price} apenas na promoção";
+//Console.WriteLine(texto);
+
+//-----------------Comparação de strings-----------------
+
