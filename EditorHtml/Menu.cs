b96493b7c8b7057070e904bd5d769af6c39ebe0c
DevAlexandreMuniz <alexandremuniz20188@gmail.com>
2022-04-19 12:03:48 -0400

    atualizei editor html

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -3,5 +3,31 @@ public static class Menu{
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
+
+        DrawnScreen();
+    }
+    public static void DesnharLinha(){
+        Console.Write("+");
+        for (int i=0; i <= 30; i++)
+            Console.Write("-");
+
+        Console.Write("+");
+        Console.Write("\n");
+    }
+    public static void DesnharColuna(){
+        for (int linha=0; linha <=10; linha ++){
+            Console.Write("|");
+            for (int i=0; i <= 30; i++)
+                Console.Write(" ");
+
+            Console.Write("|");
+            Console.Write("\n");
+        }   
+    }
+
+    public static void  DrawnScreen(){
+        DesnharLinha();
+        DesnharColuna();
+        DesnharLinha();
     }
 }
